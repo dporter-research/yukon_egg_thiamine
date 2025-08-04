131540d16b716152bbe65ee1cee7f0adbadd7456
dporter-research <dporter13@alaska.edu>
2025-08-04 15:31:28 -0800

    joing 2023 PIST genetics data

--- scripts/001-clean-data.R
+++ scripts/001-clean-data.R
@@ -4,6 +4,9 @@ library(tidyverse)
 # Load raw data
 raw_2023_df <- read_rds("data/raw/yukon_egg_thiamine_2023.rds")
 
+# Load PIST genetic assignment
+pist_2023_genetics <- read_csv("data/raw/2023_yukon_PIST_genetic_assignment.csv")
+
 # Calculate estimated protein and total mass of each component
 # Note: Protein is estimated by difference, a hypothesis to be confirmed.
 clean_2023_df <- raw_2023_df |>
@@ -19,6 +22,26 @@ clean_2023_df <- raw_2023_df |>
     water_g = g_egg * pct_moisture,
     protein_g_est = g_egg * pct_dry_matter * pct_protein_est_dry
   )
+
+# Clean PIST genetic assignment data
+pist_2023_genetics <- pist_2023_genetics |> 
+  mutate(new_ID = paste0("PIST23_", sprintf("%03d", ID))) |> 
+  select(new_ID, Country, `Broad Group`, `Fine Group`) |> 
+  rename(
+    id = new_ID,
+    country = Country,
+    broad_group = `Broad Group`,
+    fine_group = `Fine Group`
+  ) |> 
+  mutate(
+    country = factor(country),
+    broad_group = factor(broad_group),
+    fine_group = factor(fine_group)
+  )
+
+# Join PIST genetic assignment data to whole dataset
+clean_2023_df <- clean_2023_df |> 
+  left_join(pist_2023_genetics, join_by(SIN == id))
 
 # Write clean data
 write_rds(clean_2023_df, "data/processed/clean_2023_data.rds")
