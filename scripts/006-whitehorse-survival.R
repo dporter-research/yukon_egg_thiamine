00500c4f55dd06c086df458cf7aedbb7b79db061
dporter-research <dporter13@alaska.edu>
2025-08-11 16:40:25 -0800

    WH egg survival exploration

---
+++ scripts/006-whitehorse-survival.R
@@ -0,0 +1,93 @@
+library(tidyverse)
+library(ggridges)
+library(ggpubr)
+library(ggpmisc)
+library(psych)
+library(drc)
+
+## Global ggplot options -------------------------------------------------------
+
+color_palette <- c("#1F77B4FF", "#FF7F0EFF", "#2CA02CFF", "#D62728FF", "#9467BDFF",
+                   "#8C564BFF", "#E377C2FF", "#7F7F7FFF", "#BCBD22FF", "#17BECFFF")
+
+options(ggplot2.discrete.colour = color_palette)
+options(ggplot2.discrete.fill = color_palette)
+
+theme_set(theme_bw())
+
+jitter_width = 0.1
+
+## Load data -------------------------------------------------------------------
+wh_2023 <- read_rds("data/processed/clean_2023_data.rds") |> 
+  filter(site == "WH")
+
+# Load WH hatchery data
+hatchery_2023 <- read_csv("data/raw/2023 Whitehorse Egg Survival.csv") |> 
+  select(
+    `Hatchery Fish ID`, `Egg Take Date`, `% Survival Green-Eyed`, 
+    `% Survival: Green - Hatch`, `% Survival: Green - Ponding`, 
+    `Female Notes`, `Pre-eyed Morts`, `Total Eyed Eggs`, `Green Or Eyed Egg Donations`,
+    `Total at Hatched Stage`, `Hatch to Ponding Morts`, `Total at Ponding`
+  ) |> 
+  rename(
+   WH_hatchery_id = `Hatchery Fish ID`,
+   WH_egg_take_date  = `Egg Take Date`,
+   WH_surv_green_eyed = `% Survival Green-Eyed`,
+   WH_surv_green_hatch = `% Survival: Green - Hatch`,
+   WH_surv_green_pond = `% Survival: Green - Ponding`,
+   WH_female_notes = `Female Notes`,
+   pre_eyed_morts = `Pre-eyed Morts`,
+   tot_eyed_eggs = `Total Eyed Eggs`,
+   egg_donations = `Green Or Eyed Egg Donations`,
+   total_hatched = `Total at Hatched Stage`,
+   hatch_ponding_morts = `Hatch to Ponding Morts`,
+   tot_ponding = `Total at Ponding`
+  ) |> 
+  drop_na(
+    WH_hatchery_id
+  ) |> 
+  mutate(
+    tot_egg = tot_eyed_eggs + pre_eyed_morts,
+    tot_survived = tot_ponding,
+    tot_morts = tot_egg - tot_survived,
+    surv_pct = tot_survived / tot_egg
+  )
+
+# Join
+wh_2023 <- wh_2023 |> 
+  left_join(hatchery_2023)
+
+# Make long
+wh_2023_long <- wh_2023 |> 
+  select()
+
+# Examine distribution of variables --------------------------------------------
+## Egg nmol T
+ggplot(data = wh_2023, aes(x = nmol_T_egg)) +
+  geom_density() +
+  geom_rug()
+
+
+
+
+    
+##
+ggplot(data = wh_2023) +
+  geom_point(aes(x = nmol_T_egg, y = surv_pct))
+
+# 4 param model
+
+model_fit <- drm(
+  surv_pct ~ nmol_T_egg,
+  weights = tot_egg,
+  data = wh_2023,
+  fct = LL.2(),
+  type = "binomial"
+)
+
+summary(model_fit)
+
+ed(model_fit)
+
+
+?drm
